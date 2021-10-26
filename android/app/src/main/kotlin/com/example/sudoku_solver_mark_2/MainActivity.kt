25d9c2ff837cbe05cf6e931577746898180bffe9
Piyush Nagpal <85029306+zsiecr@users.noreply.github.com>
2021-10-26 23:40:56 +0530

    basic setup

---
+++ android/app/src/main/kotlin/com/example/sudoku_solver_mark_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sudoku_solver_mark_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Flutter_main_file\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter Dev App\hobby_projects\sudoku_solver_mark_2"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
